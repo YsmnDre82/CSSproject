bd81edb20d7a545e4a2e1eb9d438746608e7d8a9
YsmnDre82 <yasamandaraei1382@gmail.com>
2025-08-16 21:57:31 +0330

    add button to toggle Dropdown menu

--- script.js
+++ script.js
@@ -1,23 +1,5 @@
-const section = document.querySelector('.sec');
-const button = document.querySelector('.btn');
-const bgText = document.querySelector('.bg-text');
-const circle = document.querySelector('.cliPath-circle');
-const inputFocus = document.querySelector('.newColor-input');
+const dropdown = document.querySelector('.dropdown');
 
-let newColor;
-
-button.addEventListener('click', (e)=>{
-    e.preventDefault();
-    newColor = document.querySelector('.newColor-input').
-    value;
-    section.style.background = newColor;
-    circle.style.background = newColor;
-    circle.classList.add('active');
-    bgText.classList.add('fade');
-    bgText.innerHTML = newColor;
-});
-
-inputFocus.addEventListener('focus', ()=>{
-    circle.classList.remove('active');
-    bgText.classList.remove('fade');
-});
+dropdown.addEventListener('click',()=>{
+    dropdown.classList.toggle('active');
+})
